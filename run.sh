b792c624b1725c0375d9ac53012442297d2f73d7
wenjun <wjyan@ion.ac.cn>
2015-07-12 17:57:32 +0800

    completed

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # example of the run script for running the word count
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-python ../src/AnalyzeTweet.py ../tweet_input/tweets.txt ../tweet_output/ft1.txt ../tweet_output/ft2.txt
+python ./src/AnalyzeTweet.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt ./tweet_output/ft2.txt
